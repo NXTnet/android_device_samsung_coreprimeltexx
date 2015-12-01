23fa06db7d7b1695d93ab4ea7d6b5c0665c2fb05
Alexandru Stefan Branetu <stefan.branetu@nxtnet.ro>
2015-12-01 05:54:25 +0200

    moved to common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,163 +20,17 @@
 # definition file).
 #
 
-# inherit from qcom-common
--include device/samsung/qcom-common/BoardConfigCommon.mk
+# Inherit from common coreprimelte
+-include device/samsung/coreprimelte-common/BoardConfigCommon.mk
 
-LOCAL_PATH := device/samsung/coreprimeltexx
-
-# Platform
-TARGET_BOARD_PLATFORM := msm8916
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno306
-TARGET_BOOTLOADER_BOARD_NAME := MSM8916
-
-# Arch
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_CPU_VARIANT := cortex-a53
-TARGET_CPU_CORTEX_A53 := true
-
-TARGET_SPECIFIC_HEADER_PATH := device/samsung/coreprimeltexx/include
-
-# Kernel
-BOARD_CUSTOM_BOOTIMG_MK      := device/samsung/coreprimeltexx/mkbootimg.mk
-BOARD_KERNEL_CMDLINE         := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive
-BOARD_KERNEL_BASE            := 0x80000000
-BOARD_RAMDISK_OFFSET         := 0x02000000
-BOARD_KERNEL_TAGS_OFFSET     := 0x01e00000
-BOARD_KERNEL_SEPARATED_DT    := true
-BOARD_KERNEL_PAGESIZE        := 2048
-TARGET_KERNEL_SOURCE         := kernel/samsung/msm8916-caf
-TARGET_KERNEL_CONFIG         := cyanogen_rossa_eur_defconfig
-TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
+# Inherit from the proprietary version
+-include vendor/samsung/coreprimelte/BoardConfigVendor.mk
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := coreprimelte,coreprimeltexx,SM-G360F
 
-# Partition sizes
-TARGET_USERIMAGES_USE_EXT4 := true
-#TARGET_USERIMAGES_USE_F2FS := true
-BOARD_BOOTIMAGE_PARTITION_SIZE := 13631488
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 15728640
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1572864000
-BOARD_PERSISTIMAGE_PARTITION_SIZE := 8388608
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 5653901312
-BOARD_FLASH_BLOCK_SIZE := 131072
-
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/coreprimeltexx/bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_QCOM := true
-BLUETOOTH_HCI_USE_MCT := true
-
-# Custom RIL class
-BOARD_RIL_CLASS := ../../../device/samsung/coreprimeltexx/ril/
-
-# NFC
-BOARD_HAVE_NFC := true
-
-# Fonts
-EXTENDED_FONT_FOOTPRINT := true
-
-# malloc implementation
-MALLOC_IMPL := dlmalloc
-
-# Audio
-BOARD_USES_ALSA_AUDIO := true
-AUDIO_FEATURE_LOW_LATENCY_PRIMARY := true
-
-# Charger
-BOARD_CHARGER_SHOW_PERCENTAGE := true
-BOARD_CHARGER_ENABLE_SUSPEND := true
-
-# Enable QCOM FM feature
-TARGET_QCOM_NO_FM_FIRMWARE := true
-AUDIO_FEATURE_ENABLED_FM := true
-
-# Enable HW based full disk encryption
-TARGET_HW_DISK_ENCRYPTION := true
-
-# Build our own PowerHAL
-TARGET_POWERHAL_SET_INTERACTIVE_EXT := $(LOCAL_PATH)/power/power_ext.c
-TARGET_POWERHAL_VARIANT := qcom
-
-# Wifi
-BOARD_HAS_QCOM_WLAN 		 := true
-BOARD_HAS_QCOM_WLAN_SDK 	 := true
-BOARD_HOSTAPD_DRIVER 		 := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB 	 := lib_driver_cmd_qcwcn
-BOARD_WLAN_DEVICE 		 := qcwcn
-BOARD_WPA_SUPPLICANT_DRIVER 	 := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
-TARGET_PROVIDES_WCNSS_QMI        := true
-TARGET_USES_QCOM_WCNSS_QMI 	 := true
-TARGET_USES_WCNSS_CTRL 		 := true
-WIFI_DRIVER_FW_PATH_AP 		 := "ap"
-WIFI_DRIVER_FW_PATH_STA 	 := "sta"
-WPA_SUPPLICANT_VERSION 		 := VER_0_8_X
-
-# Vold
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
-BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
-BOARD_VOLD_MAX_PARTITIONS := 65
-
-# Camera
-TARGET_PROVIDES_CAMERA_HAL := true
-USE_DEVICE_SPECIFIC_CAMERA := true
-
-# CMHW
-BOARD_HARDWARE_CLASS += device/samsung/coreprimeltexx/cmhw
-
-# Workaround to avoid issues with legacy liblights on QCOM platforms
-TARGET_PROVIDES_LIBLIGHT := true
-
-# Qcom
-BOARD_USES_QC_TIME_SERVICES := true
-TARGET_USES_QCOM_BSP := true
-TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
-PROTOBUF_SUPPORTED := true
-COMMON_GLOBAL_CFLAGS += -DQCOM_BSP
-
-# Media
-TARGET_QCOM_MEDIA_VARIANT := caf
-TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
-
-# Display
-TARGET_CONTINUOUS_SPLASH_ENABLED := true
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-MAX_EGL_CACHE_SIZE := 2048*1024
-OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
-
-# ANT+
-BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
-
-# SELinux
-include device/qcom/sepolicy/sepolicy.mk
-
-BOARD_SEPOLICY_DIRS += \
-    device/samsung/coreprimeltexx/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-    vold.te \
-    file.te \
-    wcnss_service.te \
-    file_contexts
-
-# Misc.
-TARGET_SYSTEM_PROP := device/samsung/coreprimeltexx/system.prop
-
-# Keys
-BOARD_HAS_NO_SELECT_BUTTON := true
-
-# Storage
-TARGET_RECOVERY_FSTAB := device/samsung/coreprimeltexx/rootdir/fstab.qcom
-
-# Dex
-ifeq ($(HOST_OS),linux)
-  ifeq ($(TARGET_BUILD_VARIANT),user)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
+# Kernel
+#TARGET_KERNEL_CONFIG := cyanogen_rossa_eur_defconfig
+TARGET_KERNEL_CONFIG := msm8916_sec_defconfig
+TARGET_KERNEL_TIMA_CONFIG := tima8916_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_rossa_eur_defconfig
--- full_coreprimeltexx.mk
+++ full_coreprimeltexx.mk
@@ -14,6 +14,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit from coreprimeltexx device
 $(call inherit-product, device/samsung/coreprimeltexx/device.mk)
 
