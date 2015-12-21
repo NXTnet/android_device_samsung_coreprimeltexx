8c439f45928e274ab9b4cf164273806fcd1252d6
Stefan <stefan.branetu@nxtnet.ro>
2015-12-21 17:25:11 +0200

    switch to caf kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@
 TARGET_OTA_ASSERT_DEVICE := coreprimelte,coreprimeltexx,SM-G360F
 
 # Kernel
-#TARGET_KERNEL_CONFIG := cyanogen_rossa_eur_defconfig
-TARGET_KERNEL_CONFIG := msm8916_sec_defconfig
-TARGET_KERNEL_TIMA_CONFIG := tima8916_defconfig
-TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_rossa_eur_defconfig
+TARGET_KERNEL_CONFIG := cyanogen_rossa_eur_defconfig
+#TARGET_KERNEL_CONFIG := msm8916_sec_defconfig
+#TARGET_KERNEL_TIMA_CONFIG := tima8916_defconfig
+#TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_rossa_eur_defconfig
